03a5ae7739e773799e756789b62ee59fc7c298ff
Silvana <silvanaspadafore@gmail.com>
2017-04-07 00:15:22 -0300

    Primera version

---
+++ main.js
@@ -0,0 +1,12 @@
+var Meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+
+var Cumple = prompt("¿En qué mes cumples años?")
+
+for (var i = 0; i <12; i++){
+ 
+		if (Meses[i]!==Cumple){
+		     console.log(Meses[i])
+     }
+
+  }
+
